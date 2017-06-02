8c0df52ac862bc8fdc23e280eafe87586f8093b6
Maitri Chattopadhyay <mach5953@colorado.edu>
2017-06-01 19:59:29 -0600

    Add files via upload

---
+++ JAVA_Swing/tester.java
@@ -0,0 +1,22 @@
+package swing;
+
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+public class tester {
+
+
+   public static void main(String[] args) {
+	   
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+               View v = new View();
+              
+               
+               
+               
+            }
+        });
+   }
+}
